e5b984d62bf8e98005145a563c31038e4ebd9274
Rubina <rubinasubedi18@gmail.com>
2026-06-16 16:18:15 +0545

    add: js variables

---
+++ 1.variable.js
@@ -0,0 +1,21 @@
+console.log("Hello World");
+//!dynamic typed, interpreted & single threaded language
+var a= 12;
+console.log(a);
+a=15;
+console.log(a);
+a="bs"
+console.log(a);
+var a=78;
+console.log(a);
+//let
+let b=13;
+b=46;
+b="";
+ b=true;
+
+ const c=34;
+ c=56;
+let d;
+var e;
+const f="";
